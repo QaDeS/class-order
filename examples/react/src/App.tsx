b78d2cc73d3aab06ed4c3620a1adcd23262944f4
Michael Klaus <michael.klaus@gmx.net>
2023-08-08 05:41:56 +0200

    Proper handling of arbitrary properties

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -7,13 +7,13 @@ function App() {
 
   return (
     <>
-      <h1 className="px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10">
+      <h1 className="m-1 px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10 [margin:2rem]">
         Default behavior
       </h1>
-      <h1 className={merge("px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10")}>
+      <h1 className={merge("m-1 px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10 [margin:2rem]")}>
         With merge
       </h1>
-      <h1 className={twMerge("px-2 py-1 bg-red-300 hover:bg-blue-300", "!p-5 p-3 bg-[#FF1C1C] text-lg/10")}>
+      <h1 className={twMerge("m-1 px-2 py-1 bg-red-300 hover:bg-blue-300", "!p-5 p-3 bg-[#FF1C1C] text-lg/10 [margin:2rem]")}>
         With twMerge
       </h1>
     </>
--- examples/svelte/src/routes/+page.svelte
+++ examples/svelte/src/routes/+page.svelte
@@ -3,12 +3,12 @@
     import { twMerge } from 'tailwind-merge'
 </script>
 
-<h1 class="px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10">
+<h1 class="m-1 px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10 [margin:2rem]">
   Default behavior
 </h1>
-<h1 use:classOrder class="px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10">
+<h1 use:classOrder class="m-1 px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C] text-lg/10 [margin:2rem]">
     With use:classOrder
 </h1>
-<h1 class={twMerge("px-2 py-1 bg-red-300 hover:bg-blue-300", "!p-5 p-3 bg-[#FF1C1C] text-lg/10")}>
+<h1 class={twMerge("m-1 px-2 py-1 bg-red-300 hover:bg-blue-300", "!p-5 p-3 bg-[#FF1C1C] text-lg/10 [margin:2rem]")}>
   With twMerge
 </h1>
