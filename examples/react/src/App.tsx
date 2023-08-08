15bc42ff9c59bb453fe7f99da9de295d26a5a555
Michael Klaus <michael.klaus@gmx.net>
2023-08-08 05:30:05 +0200

    see previous; important class handling

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -7,13 +7,13 @@ function App() {
 
   return (
     <>
-      <h1 className="px-2 py-1 bg-red-300 hover:bg-blue-300 p-3 bg-[#FF1C1C]">
+      <h1 className="px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C]">
         Default behavior
       </h1>
-      <h1 className={merge("px-2 py-1 bg-red-300 hover:bg-blue-300 p-3 bg-[#FF1C1C]")}>
+      <h1 className={merge("px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C]")}>
         With merge
       </h1>
-      <h1 className={twMerge("px-2 py-1 bg-red-300 hover:bg-blue-300", "p-3 bg-[#FF1C1C]")}>
+      <h1 className={twMerge("px-2 py-1 bg-red-300 hover:bg-blue-300", "!p-5 p-3 bg-[#FF1C1C]")}>
         With twMerge
       </h1>
     </>
--- examples/svelte/src/routes/+page.svelte
+++ examples/svelte/src/routes/+page.svelte
@@ -3,7 +3,7 @@
     import { twMerge } from 'tailwind-merge'
 </script>
 
-<h1 class="px-2 py-1 bg-red-300 hover:bg-blue-300 p-3 bg-[#FF1C1C]">
+<h1 class="px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C]">
   Default behavior
 </h1>
 <h1 use:classOrder class="px-2 py-1 bg-red-300 hover:bg-blue-300 !p-5 p-3 bg-[#FF1C1C]">
