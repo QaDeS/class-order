a95340ae5f86f5f2c0ead476cf465ab139a2c48c
Michael Klaus <michael.klaus@gmx.net>
2023-08-11 01:42:45 +0200

    Partial masking

--- src/index.js
+++ src/index.js
@@ -86,20 +86,34 @@ function forceMerge (str) {
 }
 
 /**
+ * 
+ * @returns {any} the generated class
+ */
+function defineClass () {
+    const name = `class_order_${Math.floor(Math.random() * 999999)}` // TODO use hash instead?
+    const style = document.createElement('style')
+    style.innerHTML = `.${name} {}`
+    document.head.appendChild(style)
+    return {name, style: style.sheet.cssRules[0].style}
+}
+
+/**
  * @param {string} str
  * @param {boolean} override
+ * @param {HTMLElement | undefined} el
  * @returns {string}
  */
-function mergeInternal (str, override) {
+function mergeInternal (str, override, el = undefined) {
     if (import.meta.env.DEV) init()
+    console.log("\n", str, override)
 
     let cls = str.split(' ')
         .filter(Boolean) // ignore excessive whitespaces
 
-    const result = []
-    const /** @type {string[]} */ definedProps = []
+    const /** @type {string[]} */ result = []
+    const /** @type {Record<String, string>} */ definedProps = {}
     const /** @type {string[]} */ importantProps = []
-    let c
+    let /** @type {string} */ c
     while ((c = cls.pop())) {
         const props = classProps[c]
         if (!props) {
@@ -107,17 +121,66 @@ function mergeInternal (str, override) {
             continue
         }
 
-        const newProps = props.properties.filter((p) => !definedProps.includes(p))
+        const newProps = props.properties.filter((p) => !definedProps[p])
         const overriding = props.importantProperties.filter((p) => !importantProps.includes(p))
-        if (newProps.length ||
-            (!override && (props.importantProperties.length && overriding.length))
-        ) {
+
+        const registerClass = () => {
             result.unshift(c)
-            definedProps.push(...newProps)
+            newProps.forEach((p) => definedProps[p] = c)
             importantProps.push(...overriding)
+        }
+
+        if (newProps.length == props.properties.length) {
+            // no conflicts
+            registerClass()
+        } else if (override) {
+            if (props.importantProperties.length > newProps.length) {
+                if (newProps.length) {
+                    const overridden = [...new Set(props.importantProperties.map((p) => definedProps[p]).filter(Boolean))]
+
+                    let e = el
+                    if (!el) {
+                        console.warn("No Element, estimating style for", c)
+                        e = document.createElement('div')
+                        document.body.appendChild(e)
+                    }
+
+                    e.className = result.join(' ')
+                    const s = getComputedStyle(e)
+
+                    const {name, style} = defineClass()
+                    props.importantProperties.filter((p) => definedProps[p])
+                        .forEach((p) => style.setProperty(p, s.getPropertyValue(p), 'important'))
+
+                    if(!el) document.body.removeChild(e)
+
+                    // FIXME
+                    // overridden.forEach((o) => result.splice(result.indexOf(o)))
+
+                    registerClass()
+                    result.push(name)
+
+                    console.warn("Overriding", c, "with", name)
+                } else {
+                    const overridden = [...new Set(props.properties.map((p) => definedProps[p]))]
+                    console.warn("Override", c, ...overridden)
+                }
+
+            }
+        } else {
+            if (props.importantProperties.length == props.properties.length - newProps.length) {
+                // inly important new properties
+                registerClass()
+            } else if (props.importantProperties.length && (props.importantProperties.length == overriding.length)) {
+                // no important Conflicts
+                registerClass()
+            } else {
+                console.warn("Deleting", c)
+            }
         }
     }
 
+    console.log(result.join(' '))
     return result.join(' ')
 }
 
@@ -126,13 +189,13 @@ function mergeInternal (str, override) {
  * @param {AugmentedElement} el 
  * @returns 
  */
-function classOrder (el, opts = {override: false}) {
-    const {override} = opts
-    el.className = mergeInternal(el.className, override)
+function classOrder (el, opts = { override: false }) {
+    const { override } = opts
+    el.className = mergeInternal(el.className, override, el)
     el._setAttribute = el.setAttribute
     el.setAttribute = (k, v) => {
         if (k != 'class') return el._setAttribute(k, v)
-        el._setAttribute(k, mergeInternal(v, override))
+        el._setAttribute(k, mergeInternal(v, override, el))
     }
     return {
         destroy () {
@@ -147,8 +210,8 @@ function classOrder (el, opts = {override: false}) {
  * @param {AugmentedElement} el 
  * @returns 
  */
-function forceClassOrder(el) {
-    return classOrder(el, {override: true})
+function forceClassOrder (el) {
+    return classOrder(el, { override: true })
 }
 
 export {
