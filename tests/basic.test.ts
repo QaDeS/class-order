b70bbddc5cf45f6998c2db975d697afb984689f9
Michael Klaus <michael.klaus@gmx.net>
2023-08-11 06:26:40 +0200

    disabled puppeteer tests for now

--- tests/basic.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'bun:test'
-
-describe("Basics", () => {
-    it('proceses overwritten attributes', () => {
-        expect(!!document.body).toBe(true)
-    })
-})
